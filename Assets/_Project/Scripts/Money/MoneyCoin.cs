e56bbc793f1df9c4735fc0880ef5a8ceac4e868e
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2024-02-23 00:28:30 +0200

    magnets for gold coins reworked

--- Assets/_Project/Scripts/Money/MoneyCoin.cs
+++ Assets/_Project/Scripts/Money/MoneyCoin.cs
@@ -1,7 +1,8 @@
-﻿using DG.Tweening;
-using gishadev.fort.Core;
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using gishadev.fort.Money.Magnet;
 using UnityEngine;
-using Zenject;
 
 namespace gishadev.fort.Money
 {
@@ -9,40 +10,82 @@ namespace gishadev.fort.Money
     public class MoneyCoin : MonoBehaviour
     {
         [SerializeField] private int moneyToAdd = 5;
-
-        [Inject] private IMoneyController _moneyController;
+        [SerializeField] private float minIdleTimeBeforeJump = 0.5f;
+        [SerializeField] private float maxIdleTimeBeforeJump = 1f;
 
         public bool IsMagnetized { get; private set; }
 
-        private Rigidbody _rb;
+        public int MoneyToAdd => moneyToAdd;
 
-        private void Awake() => _rb = GetComponent<Rigidbody>();
+        private Rigidbody _rb;
+        private ICoinMagnet _nearestMagnet;
+        private CancellationTokenSource _cts;
+
+        private async void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _cts = new CancellationTokenSource();
+            _cts.RegisterRaiseCancelOnDestroy(gameObject);
+
+            var timeBeforeJump = Random.Range(minIdleTimeBeforeJump, maxIdleTimeBeforeJump);
+            await UniTask.WaitForSeconds(timeBeforeJump, cancellationToken: _cts.Token)
+                .SuppressCancellationThrow();
+            if (_cts.IsCancellationRequested)
+                return;
+
+            _nearestMagnet = FindNearestMagnet();
+            JumpTo(_nearestMagnet.transform);
+        }
 
         public void JumpTo(Transform targetTrans)
         {
             _rb.isKinematic = true;
             // Follow until it reaches target trans
-            transform.DOJump(targetTrans.position, 1f, 1, 0.3f).SetEase(Ease.OutCubic)
+            transform
+                .DOJump(targetTrans.position, 1f, 1, 0.3f)
+                .SetEase(Ease.OutCubic)
                 .OnComplete(() => FastJumpTo(targetTrans));
             IsMagnetized = true;
         }
 
         private void FastJumpTo(Transform targetTrans)
         {
-            transform.DOMove(targetTrans.position, 0.1f).OnComplete(Collect);
+            transform.DOMove(targetTrans.position, 0.1f)
+                .OnComplete(() => Collect(_nearestMagnet));
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag(Constants.PLAYER_TAG_NAME))
-                Collect();
+            if (other.TryGetComponent(out ICoinMagnet magnet))
+                Collect(magnet);
         }
 
-        private void Collect()
+        private void Collect(ICoinMagnet magnet)
         {
+            magnet.OnCoinCollect(this);
             DOTween.Kill(transform);
-            _moneyController.AddMoney(moneyToAdd);
             Destroy(gameObject);
+        }
+
+        private ICoinMagnet FindNearestMagnet()
+        {
+            var magnets = FindObjectsOfType<MonoBehaviour>();
+            ICoinMagnet nearestMagnet = null;
+            float nearestDistance = float.MaxValue;
+            foreach (var magnet in magnets)
+            {
+                if (magnet is ICoinMagnet coinMagnet)
+                {
+                    var distance = Vector3.Distance(transform.position, coinMagnet.transform.position);
+                    if (distance < nearestDistance)
+                    {
+                        nearestDistance = distance;
+                        nearestMagnet = coinMagnet;
+                    }
+                }
+            }
+
+            return nearestMagnet;
         }
     }
 }
